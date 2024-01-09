3c5939d958923863a219a3fd5314ef08dac27470
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-09 23:29:20 +0600

    Design for main page

--- src/pages/main/ui/main.tsx
+++ src/pages/main/ui/main.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <main>
+      <div>
+        <h1 className='text-center'>Weather</h1>
+        <div>
+          <p>WEATHER IMG</p>
+          <p>Degree</p>
+          <p>country</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- src/shared/api/WeatherApi/weatherApi.ts
+++ src/shared/api/WeatherApi/weatherApi.ts
@@ -1,18 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import axios from "axios";
-import { ITaskApi } from "./types";
-
-// export function GetTaskApi(){
-//     axios.get('http://localhost:3001/tasks')
-//     .then(res => console.log(res))
-// }
 
 export const taskApi = createApi({
     reducerPath: 'taskApi',
     tagTypes: ['Tasks'],
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
     endpoints: (builder) => ({
-      getWeather: builder.query<ITaskApi | any , string>({
+      getWeather: builder.query< any , string>({
         query: () => `tasks`,
         providesTags: (result) => result
           ? [
